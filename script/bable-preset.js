2348f6772a419efc9ebcecafed4550c15f8056a7
Thu Htet Swan Saung <thuhtetswansaung@Thus-MacBook-Air.local>
2026-02-03 15:36:50 +0700

    first commit

---
+++ script/bable-preset.js
@@ -0,0 +1,40 @@
+// const BABEL_ENV = process.env.BABEL_ENV;
+// const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs';
+// const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm';
+
+// module.exports = () => ({
+//   presets: [
+//     ['@babel/preset-typescript'],
+//     [
+//       '@babel/env',
+//       {
+//         bugfixes: true,
+//         loose: true,
+//         modules: isCommonJS ? 'commonjs' : false,
+//         targets: {
+//           esmodules: isESM ? true : undefined,
+//           chrome: 70,
+//         },
+//       },
+//     ],
+//   ],
+// });
+
+const BABEL_ENV = process.env.BABEL_ENV
+const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs'
+const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm'
+
+module.exports = ()=>({
+        preset:[
+            ['@babel/preset-typescript'],
+            ['@babel/env',{
+                bugfixs: true,
+                loose: true,
+                modules: isCommonJS? 'cjs' : false,
+                targets: {
+                    esmodules: isESM? true : undefined,
+                    chrome: 70
+                }
+            }]
+        ]
+})
