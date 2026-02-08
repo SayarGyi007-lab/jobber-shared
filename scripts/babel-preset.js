104f1539c1cf89fc382b710722a43f767de4934e
Thu Htet Swan Saung <thuhtetswansaung@Thus-MacBook-Air.local>
2026-02-08 17:51:17 +0700

    ci fix3

--- scripts/babel-preset.js
+++ scripts/babel-preset.js
@@ -25,12 +25,12 @@ const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs'
 const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm'
 
 module.exports = ()=>({
-        preset:[
+        presets:[
             ['@babel/preset-typescript'],
             ['@babel/env',{
-                bugfixs: true,
+                bugfixes: true,
                 loose: true,
-                modules: isCommonJS? 'cjs' : false,
+                modules: isCommonJS? 'commonjs' : false,
                 targets: {
                     esmodules: isESM? true : undefined,
                     chrome: 70
