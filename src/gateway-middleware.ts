7b66d0dfecb5aa2742ab388c3158c2ccc2640db2
Thu Htet Swan Saung <thuhtetswansaung@Thus-MacBook-Air.local>
2026-03-10 14:38:11 +0700

    log

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -5,15 +5,21 @@ import { NotAuthorizedError } from "./error-handler"
 const tokens: string[] = ['auth','seller','gig','search','buyer','message','order','review']
 
 export function verifyGatewayRequest(req:Request, _res:Response, next:NextFunction):void{
+    console.log('Headers:', req.headers);
     if(!req.headers?.gatewaytoken){
-        throw new NotAuthorizedError('Invalid tokne','verifyGatewayRequest():Rquest not coming')
+        throw new NotAuthorizedError('Invalid token from headers','verifyGatewayRequest():Rquest not coming')
     }
     const token:string = req.headers?.gatewaytoken as string
     if(!token){
         throw new NotAuthorizedError('No tokne','verifyGatewayRequest():No token')
     }
+    
+console.log('Gateway token:', token);
+console.log('Decoded (no verify):', jwt.decode(token));
     try {
+        
         const decoded: {id: string, iat: number} = jwt.verify(token,'DSJFAJSDOFP382238T920IODSAJoifaoshjtqwas') as {id: string, iat: number}
+        console.log('Verified payload:', decoded);
         if(!decoded){
             throw new NotAuthorizedError('No tokne','verifyGatewayRequest():No token')
         }
@@ -22,7 +28,7 @@ export function verifyGatewayRequest(req:Request, _res:Response, next:NextFuncti
         }
         
     } catch (error) {
-        throw new NotAuthorizedError('Invalid tokne','verifyGatewayRequest():Rquest not coming')
+        throw new NotAuthorizedError('Invalid token from catch block','verifyGatewayRequest():Rquest not coming')
     }
     next()
 }
